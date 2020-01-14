b54aaea31a5170b799dd1ff73092d1af3b500c16
rinehart7 <evanrinehart1@gmail.com>
2020-01-14 09:42:26 -0500

    route.py shows stl file prelim

--- route.py
+++ route.py
@@ -2,32 +2,30 @@ import re
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('TkAgg')
 
 filename=open("cube.STL")
 
-points = []
+points_x = []
+points_y = []
+points_z = []
 for line in filename:
     if "vertex" in line:
+        print(line)
         xnumber = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[0][0]
         xexponent = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[0][2]
         xreal_exponent = int(re.findall(r'\d+', xexponent)[0])
-        point_x = float(xnumber)*10**xreal_exponent
+        points_x.append(float(xnumber)*10**xreal_exponent)
 
         ynumber = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[1][0]
         yexponent = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[1][2]
         yreal_exponent = int(re.findall(r'\d+', yexponent)[0])
-        point_y = float(ynumber)*10**yreal_exponent
+        points_y.append(float(ynumber)*10**yreal_exponent)
 
-        znumber = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[1][0]
-        zexponent = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[1][2]
+        znumber = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[2][0]
+        zexponent = re.findall(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?', line )[2][2]
         zreal_exponent = int(re.findall(r'\d+', zexponent)[0])
-        point_z = float(znumber)*10**zreal_exponent
-        lst=[point_x, point_y, point_z]
-        points.append(lst)
+        points_z.append(float(znumber)*10**zreal_exponent)
 
-fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.scatter3D(points[0][0], points[0][1], points[0][2], 'gray')
-plt.show
+ax.plot3D(points_x, points_y, points_z,'--')
+plt.show()
